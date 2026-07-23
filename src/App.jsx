eab2c28ea8d53cd340c162db6cab7eb852a17f48
Emanuel002-code <tumelomafalo77@gmail.com>
2026-07-23 11:44:11 +0200

    Update Marks analysis

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import CreateAccount from './pages/CreateAccount';
 import SignIn from './pages/SignIn';
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import CareerGuidance from "./pages/CareerGuidance.jsx";
+import MarksAnalysis from "./pages/MarksAnalysis.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/create-account" />} />
-        <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/marks-analysis" element={<MarksAnalysis />} />
+          <Route path="/career-guidance" element={<CareerGuidance />} />
+          <Route path="/create-account" element={<CreateAccount />} />
       </Routes>
     </BrowserRouter>
   );
